ea13bcf2c77286c9517dd2ae250b7ab5146eb804
ginxo <emingora@redhat.com>
2021-05-10 11:56:13 +0200

    [BXMSPROD-1311] skip error on pull_request event

--- action.js
+++ action.js
@@ -3,8 +3,6 @@ const github = require('@actions/github');
 const { parseTestReports } = require('./utils.js');
 
 const action = async () => {
-    core.info(`[DEBUG] 1`);
-
     const reportPaths = core.getInput('report_paths').split(',').join('\n');
     core.info(`Going to parse results form ${reportPaths}`);
     const githubToken = core.getInput('github_token');
@@ -12,51 +10,52 @@ const action = async () => {
     const commit = core.getInput('commit');
     const failOnFailedTests = core.getInput('fail_on_test_failures') === 'true';
     const failIfNoTests = core.getInput('fail_if_no_tests') === 'true';
+    const skipCheckRun = core.getInput('skip_check_run') === 'true';
 
     let { count, skipped, annotations } = await parseTestReports(reportPaths);
     const foundResults = count > 0 || skipped > 0;
-    const title = foundResults
-        ? `${count} tests run, ${skipped} skipped, ${annotations.length} failed.`
-        : 'No test results found!';
-    core.info(`Result: ${title}`);
-
-    const pullRequest = github.context.payload.pull_request;
-    const link = (pullRequest && pullRequest.html_url) || github.context.ref;
     const conclusion =
         (foundResults && annotations.length === 0) || (!foundResults && !failIfNoTests)
             ? 'success'
             : 'failure';
-    const status = 'completed';
-    const head_sha = commit || (pullRequest && pullRequest.head.sha) || github.context.sha;
-    core.info(
-        `Posting status '${status}' with conclusion '${conclusion}' to ${link} (sha: ${head_sha})`
-    );
 
-    core.info(`[DEBUG] 2`);
-    const createCheckRequest = {
-        ...github.context.repo,
-        name,
-        head_sha,
-        status,
-        conclusion,
-        output: {
-            title,
-            summary: '',
-            annotations: annotations.slice(0, 50)
-        }
-    };
+    if (!skipCheckRun) {
+        const title = foundResults
+            ? `${count} tests run, ${skipped} skipped, ${annotations.length} failed.`
+            : 'No test results found!';
+        core.info(`Result: ${title}`);
 
-    core.debug(JSON.stringify(createCheckRequest, null, 2));
+        const pullRequest = github.context.payload.pull_request;
+        const link = (pullRequest && pullRequest.html_url) || github.context.ref;
+        const status = 'completed';
+        const head_sha = commit || (pullRequest && pullRequest.head.sha) || github.context.sha;
+        core.info(
+            `Posting status '${status}' with conclusion '${conclusion}' to ${link} (sha: ${head_sha})`
+        );
+
+        const createCheckRequest = {
+            ...github.context.repo,
+            name,
+            head_sha,
+            status,
+            conclusion,
+            output: {
+                title,
+                summary: '',
+                annotations: annotations.slice(0, 50)
+            }
+        };
+
+        core.debug(JSON.stringify(createCheckRequest, null, 2));
+
+        const octokit = new github.GitHub(githubToken);
+        await octokit.checks.create(createCheckRequest);
+    }
 
     // make conclusion consumable by downstream actions
     core.setOutput('conclusion', conclusion);
 
-    const octokit = new github.GitHub(githubToken);
-    core.info(`[DEBUG] 3`);
-    await octokit.checks.create(createCheckRequest);
-
     // optionally fail the action if tests fail
-    core.info(`[DEBUG] 4`);
     if (failOnFailedTests && conclusion !== 'success') {
         core.setFailed(`There were ${annotations.length} failed tests`);
     }
