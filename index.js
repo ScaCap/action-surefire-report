5ea8bd63436c54837a3893476bfed0aa32e5a2b6
Ghais Zaher <ghaiszaher@gmail.com>
2022-12-24 19:26:31 +0100

    test to fail

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const action = require('./action');
 
 (async () => {
     try {
+        throw new Error("Can't do it")
         await action();
     } catch (error) {
         core.setFailed(error.message);
