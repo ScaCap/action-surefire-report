301770bd0a12305bce5815d8a46a224cb258ff83
Jonny Rimek <jimbo.db@protonmail.com>
2026-06-24 16:56:56 +0200

    PE-4088: avoid public Go downloads in public CI

--- integration-tests/go/utils/string_test.go
+++ integration-tests/go/utils/string_test.go
@@ -1,8 +1,9 @@
 package utils
 
 import "testing"
-import "github.com/stretchr/testify/assert"
 
 func TestFailing(t *testing.T) {
-	assert.Equal(t, "1", "2")
+	if "1" != "2" {
+		t.Error("expected 1 to equal 2")
+	}
 }
